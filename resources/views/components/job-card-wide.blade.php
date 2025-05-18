cf9ba75ff1d5172570493bea7f112af322fd9a91
Karol <karol.lucki@gmail.com>
2025-05-18 19:44:48 +0200

    Finished layout

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,22 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Firma</a>
+
+        <h3 class="mt-3 group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Stanowsiko</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">Wymiar pracy - wypłata od</p>
+    </div>
+
+    <div class="">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+    </div>
+</x-panel>
