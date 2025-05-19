5bcb41b2579ec4cf30abf7470c4579a0f39ffce8
Karol <karol.lucki@gmail.com>
2025-05-19 16:17:58 +0200

    Made home page dinamic

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,21 +1,23 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Firma</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="mt-3 group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Stanowsiko</h3>
+        <h3 class="mt-3 group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
 
-        <p class="text-sm text-gray-400 mt-auto">Wymiar pracy - wypłata od</p>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->schedule}} - {{ $job->salary }}</p>
     </div>
 
     <div class="">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag />
+            @endforeach
         </div>
 
     </div>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,18 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Firma</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Stanowsiko</h3>
-        <p class="text-sm mt-4">Wymiar pracy - wypłata od</p>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">PHP</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag size="small" />
+            @endforeach
         </div>
 
         <x-employer-logo width="42" />
